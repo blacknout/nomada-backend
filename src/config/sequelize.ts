f57b972d81de29e3d79da2cb109efc8000e2d824
obi walker <obinnawalker@gmail.com>
2025-03-22 17:14:51 +0100

    test deploy

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -10,7 +10,7 @@ console.log("DB_USER:", process.env.DB_USER);
 console.log("DB_PASS:", process.env.DB_PASS);
 
 const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASS!, {
-  host: process.env.DB_HOST || "db1tsttosee",
+  host: process.env.DB_HOST || "db",
   port: Number(process.env.DB_PORT) || 5432,
   dialect: "postgres",
   logging: false,
