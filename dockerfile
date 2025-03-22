2b31a0b3e7b4c254701eb4922df6a19e7ef71972
obi walker <obinnawalker@gmail.com>
2025-03-22 14:34:43 +0100

    fix build and deply issue

--- dockerfile
+++ dockerfile
@@ -3,9 +3,13 @@ FROM node:18-alpine
 WORKDIR /app
 
 COPY package.json package-lock.json ./
-RUN npm install
 
-COPY . . 
+RUN npm ci
+
+RUN apk add --no-cache postgresql-client
+
+COPY . .
+
 RUN npm run build
 
 CMD ["node", "dist/server.js"]
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,7 @@
-import expressListEndpoints from "express-list-endpoints";
 import app from "./app";
 import sequelize from "./config/sequelize";
 
 const PORT = process.env.PORT || 3000;
-
-console.log(expressListEndpoints(app));
 
 sequelize.sync({ force: false }).then(() => {
   console.log("Database connected");
