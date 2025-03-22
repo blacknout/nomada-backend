9cfa981a3cc28b86ef0a73993ec43c3c1754f88f
obi walker <obinnawalker@gmail.com>
2025-03-22 17:23:40 +0100

    change to db url

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -3,16 +3,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log("**********-----------************")
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_NAME:", process.env.DB_NAME);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASS:", process.env.DB_PASS);
+// const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASS!, {
+//   host: process.env.DB_HOST || "db",
+//   port: Number(process.env.DB_PORT) || 5432,
+//   dialect: "postgres",
+//   logging: false,
+// });
 
-const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASS!, {
-  host: process.env.DB_HOST || "db",
-  port: Number(process.env.DB_PORT) || 5432,
+const sequelize = new Sequelize(process.env.DATABASE_URL!, {
   dialect: "postgres",
+  dialectOptions: {
+    ssl: { require: true, rejectUnauthorized: false },
+  },
   logging: false,
 });
 
