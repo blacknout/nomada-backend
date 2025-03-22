7abd7beb9dec13b46d09227f155593e9d43b3f51
obi <obinnawalker@gmail.com>
2025-03-23 00:52:17 +0100

    remove commented code

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -3,16 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASS!, {
-//   host: process.env.DB_HOST || "db",
-//   port: Number(process.env.DB_PORT) || 5432,
-//   dialect: "postgres",
-//   logging: false,
-// });
-console.log("---------------------**************----------------------");
-
-console.log("🚀 DATABASE_URL:", process.env.DATABASE_URL);
-
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   dialectOptions: {
