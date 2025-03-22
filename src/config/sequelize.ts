d39a8a4a16884a2641103ecf1ad0a4aba13a9255
obi walker <obinnawalker@gmail.com>
2025-03-22 16:35:06 +0100

    test db host name on deploy

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASS!, {
-  host: process.env.DB_HOST || "db",  // 
+  host: process.env.DB_HOST || "db1tsttosee",  // 
   port: Number(process.env.DB_PORT) || 5432,
   dialect: "postgres",
   logging: false,
